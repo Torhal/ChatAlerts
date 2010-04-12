e7e7bb3261531c1cf2d223e3ef9a5f3d97cdaf69
James D. Callahan III <darkenelf@gmail.com>
2010-04-12 05:39:30 -0400

    Added localization support.

---
+++ Locales/devel-enUS.lua
@@ -0,0 +1,12 @@
+local ADDON_NAME, namespace = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "enUS", true)
+
+if not L then return end
+
+L["Font Colors"]	= true
+L["Active"]		= true
+L["Inactive"]		= true
+L["Alert"]		= true
+
+L["Tab Colors"]		= true
